e660e73962fe3e14b7177e62158b7b33fcc193b8
Rainers K. <you@example.com>
2024-10-13 19:21:07 +0300

    caezer algo testing

--- src/algos/caeser.cpp
+++ src/algos/caeser.cpp
@@ -2,21 +2,22 @@
 
 namespace Caeser {
 
-	std::string encrypt(std::string input, int key) {
-		std::string output;
-		for (char& c : input) {
-			if(c >= 'A' && c <= 'Z')
-				output.push_back((c - 'A' + key + 26) % 26 + 'A');
-			else if(c >= 'a' && c <= 'z')
-				output.push_back((c - 'a' + key + 26) % 26 + 'a');
-			else
-				output.push_back(c);
-		}
-		
-		return output;
+std::string encrypt(std::string input, int key) {
+	key = key % 26;
+	std::string output;
+	for (char& c : input) {
+		if(c >= 'A' && c <= 'Z')
+			output.push_back((c - 'A' + key + 26) % 26 + 'A');
+		else if(c >= 'a' && c <= 'z')
+			output.push_back((c - 'a' + key + 26) % 26 + 'a');
+		else
+			output.push_back(c);
 	}
+	
+	return output;
+}
 
-	std::string decrypt(std::string input, int key) {
-		return encrypt(input, -key);
-	}
+std::string decrypt(std::string input, int key) {
+	return encrypt(input, -key);
+}
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -15,10 +15,42 @@ TEST(Caeser, basic_test) {
 TEST(Caeser, key_zero) {
 	std::string plaintext = "This plaintext is encrypted with key zero";
 	std::string encrypted_expected =  plaintext;
-	std::string encrypted = Caeser::encrypt(plaintext, 15);
-	std::string decrypted = Caeser::decrypt(encrypted, 15);
+	std::string encrypted = Caeser::encrypt(plaintext, 0);
+	std::string decrypted = Caeser::decrypt(encrypted, 0);
 
 	EXPECT_EQ(encrypted, encrypted_expected);
 	EXPECT_EQ(decrypted, plaintext);
 }
 
+TEST(Caeser, key_alphabet_size_26) {
+	std::string plaintext = "This plaintext is encrypted witk key 26";
+	// result should be the same as plaintext because key 26 wraps each letter back to itself
+	std::string encrypted_expected =  plaintext;
+	std::string encrypted = Caeser::encrypt(plaintext, 26);
+	std::string decrypted = Caeser::decrypt(encrypted, 26);
+
+	EXPECT_EQ(encrypted, encrypted_expected);
+	EXPECT_EQ(decrypted, plaintext);
+}
+
+TEST(Caeser, key_69) {
+	std::string plaintext = "My very big secret";
+	// the result should be the same as with key 17 beacuse 69mod26 = 17
+	std::string encrypted_expected = "Dp mvip szx jvtivk";
+	std::string encrypted = Caeser::encrypt(plaintext, 69);
+	std::string decrypted = Caeser::decrypt(encrypted, 69);
+
+	EXPECT_EQ(encrypted, encrypted_expected);
+	EXPECT_EQ(decrypted, plaintext);
+}
+
+TEST(Caeser, plaintext_gibberish) {
+	std::string plaintext = "My  very big  0743 secret &&^^ d2f";
+	// only english letters should change, everything else should remain the same as plaintext
+	std::string encrypted_expected = "Gs  pyls vca  0743 mywlyn &&^^ x2z";
+	std::string encrypted = Caeser::encrypt(plaintext, 20);
+	std::string decrypted = Caeser::decrypt(encrypted, 20);
+
+	EXPECT_EQ(encrypted, encrypted_expected);
+	EXPECT_EQ(decrypted, plaintext);
+}
